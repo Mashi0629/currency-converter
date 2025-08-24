eb2b26a3e6b8805bb3b12cae36544734cacc73de
Mashi0629 <poboshiniomashi@gmail.com>
2025-08-24 21:32:06 +0530

    update print statement
     print full API response

--- currency_converter.py
+++ currency_converter.py
@@ -4,6 +4,9 @@ def convert_currency(amount, from_currency, to_currency):
     url = f"https://api.exchangerate.host/convert?from={from_currency}&to={to_currency}&amount={amount}"
     response = requests.get(url)
     data = response.json()
+
+    print(data)
+    
     if data.get("result"):
         return data["result"]
     
@@ -12,10 +15,5 @@ def convert_currency(amount, from_currency, to_currency):
     
 
     
-amount =float(input("Enter amount: "))
-from_currency = input("From currency (e.g., USD): ").upper()
-to_currency = input("To currency (e.g., LKR): ").upper()
 
-result = convert_currency(amount, from_currency, to_currency)
-print(f"{amount} {from_currency} = {result} {to_currency}")
 
